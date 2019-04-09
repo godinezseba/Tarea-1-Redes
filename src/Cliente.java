635a565e62869b46af77abba0de06d26c6b24c0c
Sebastian Godinez <sebastian.godinez@sansano.usm.cl>
2019-04-09 18:05:57 -0400

    agregue el envio de archivos

--- src/Cliente.java
+++ src/Cliente.java
@@ -3,8 +3,12 @@ import java.io.PrintStream;
 import java.util.Scanner;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
+import java.io.File;
+import java.io.FileInputStream;
 public class Cliente {
     public static void main(String[] args) throws IOException {
         String mensaje;
@@ -13,6 +17,13 @@ public class Cliente {
         // entrada y salida de datos
         Scanner entradaDatos = new Scanner(cs.getInputStream()); // entrada
         PrintStream salidaDatos = new PrintStream(cs.getOutputStream()); // salida
+        InputStream in = null;
+        OutputStream out = null;
+        // abro el archivo
+        File file = new File("./../files/test.txt");
+        long length = file.length();
+
+        byte[] bytes = new byte[16*1024];
 
         // envio un mensaje
         salidaDatos.println("Cliente: Hola soy un cliente!");
@@ -21,7 +32,20 @@ public class Cliente {
         mensaje = entradaDatos.nextLine();
         System.out.println(mensaje);        
         
+        // envio el archivo
+        in = new FileInputStream(file);
+        out = cs.getOutputStream();
+
+        int count;
+        while((count = in.read(bytes)) > 0){
+            out.write(bytes, 0, count);
+        }
+        
         System.out.println("Fin de la conexión");
+
+        out.close();
+        in.close();
+        entradaDatos.close();
         cs.close();
     }
 }
--- src/Servidor.java
+++ src/Servidor.java
@@ -1,28 +1,52 @@
 // entrada y salida
 import java.util.Scanner;
 import java.io.PrintStream;
-
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.io.File;
+import java.io.IOException;
+import java.io.FileOutputStream;
 public class Servidor{
     public static void main(String[] args) throws IOException {
         // recibo el mensaje
         String mensaje;
+        ServerSocket ss = null;
+
         // creo los sockets
-        ServerSocket ss = new ServerSocket(1234); 
-        Socket cs = new Socket(); // comienza el socket y espera clientes
+        try{
+            ss = new ServerSocket(1234); 
+        } catch(IOException e){
+            System.err.println("No se pudo abrir el puerto");
+            //exit(-1);
+        }
+        Socket cs = null;
+        InputStream in = null;
+        OutputStream out = null;
+
+        System.out.println("Esperando...");
 
         // entrada de un cliente
-        System.out.println("Esperando...");
-        cs = ss.accept();
-        System.out.println("Cliente en línea");
+        try {
+            cs = ss.accept(); // comienza el socket y espera clientes
+            System.out.println("Cliente en línea");
+        } catch (Exception e) {
+            System.err.println("Error en la entrada de un cliente");
+        }
 
         // entrada y salida de datos
         Scanner entradaDatos = new Scanner(cs.getInputStream()); // entrada
         PrintStream salidaDatos = new PrintStream(cs.getOutputStream()); // salida
-        
+        try {
+            in = cs.getInputStream();
+        } catch (Exception e) {
+            System.err.println("No se pudo obtener el input");
+        }
+
+
         // leo un mensaje
         mensaje = entradaDatos.nextLine();
         System.out.println(mensaje);
@@ -30,8 +54,32 @@ public class Servidor{
         // envio un mensaje
         salidaDatos.println("Servidor: Hola Cliente");
 
+        // creo el archivo a guardar
+        try {
+            out = new FileOutputStream("./../files/test2.txt");
+        } catch (FileNotFoundException e) {
+            try {
+                File file = new File("./../files/test2.txt");
+                file.createNewFile();
+            } catch (Exception er) {
+                System.err.println("Error al crear el archivo");
+            }
+        }
+
+        byte[] bytes = new byte[16*1024];
+
+        int count;
+        while((count = in.read(bytes)) > 0){
+            out.write(bytes, 0, count);
+        }
         System.out.println("Fin de la conexión");
 
+        out.close();
+        in.close();
+        entradaDatos.close();
         ss.close();
+        cs.close();
+
+        //ss.close();
     }
 }
